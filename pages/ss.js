802aee43c98974e2465b67020de45f60089f4d87
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-02-13 10:36:47 +0530

    bugfix

--- pages/ss.js
+++ pages/ss.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Projectcard from "../components/Projectcard";
-import Loading from "../common/loading";
+import Loading from "../common/Loading";
 import Mainlayout from "../common/Mainlayout";
 export default function Home() {
   return (
